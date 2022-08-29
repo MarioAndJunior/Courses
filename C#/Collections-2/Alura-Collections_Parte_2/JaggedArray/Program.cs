0dbe11ada91abc7dab8b205db12c1432590ecc75
mario_junior <mario_junior@hst.com.br>
2022-08-29 09:52:56 -0300

    :sparkles: Novos capítulos do curso

---
+++ C#/Collections-2/Alura-Collections_Parte_2/JaggedArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace JaggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // família 1: Família Flintstones
+            // família 2: Família Simpsons
+            // família 3: Família Dona Florinda
+
+
+            // Jagged Array (array serrilhado)
+            string[][] familias = new string[3][];
+            //{
+            //    { "Fred", "Wilma", "Pedrita" }.
+            //    { "Homer", "Marge", "Lisa", "Bart", "Maggie" },
+            //    { "Florina", "Kiko" }
+            //};
+
+            familias[0] = new string[] { "Fred", "Wilma", "Pedrita" };
+            familias[1] = new string[] { "Homer", "Marge", "Lisa", "Bart", "Maggie" };
+            familias[2] = new string[] { "Florina", "Kiko" };
+
+            foreach (var familia in familias)
+            {
+                Console.WriteLine(string.Join(',', familia));
+            }
+        }
+    }
+}
