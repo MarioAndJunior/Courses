43c47fcf46e187d3851a7c685d60999937055cb1
mario_junior <mario_junior@hst.com.br>
2022-09-06 09:43:26 -0300

    Finalizando curso

---
+++ C#/Collections-2/Alura-Collections_Parte_2/Covariancia/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Covariancia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("string para object");
+            
+            string titulo = "meses";
+            object obj = titulo;
+
+            Console.WriteLine(obj);
+
+            Console.WriteLine("List<string> para List<object>");
+
+            IList<string> listaMeses = new List<string>()
+            {
+                "Janeiro", "Fevereiro", "Março", "Abril",
+                "Maio", "Junho", "Julho", "Agosto", 
+                "Setembro", "Outubro", "Novembro", "Dezembro",
+            };
+
+            // IList<object> listaObj = listaMeses;
+            Console.WriteLine();
+
+            Console.WriteLine("string[] para object[]?");
+
+            string[] arrayMeses = new string[]
+            {
+                "Janeiro", "Fevereiro", "Março", "Abril",
+                "Maio", "Junho", "Julho", "Agosto",
+                "Setembro", "Outubro", "Novembro", "Dezembro",
+            };
+
+            object[] arrayObj = arrayMeses; // Covariância do array. Deve ser evitada SEMPRE
+
+            foreach (var mes in arrayObj)
+            {
+                Console.WriteLine(mes);
+            }
+
+            arrayObj[0] = "PRIMEIRO MÊS";
+            Console.WriteLine(arrayObj[0]);
+            Console.WriteLine();
+
+            // arrayObj[0] = 12345;
+            // Console.WriteLine(arrayObj[0]);
+            // Console.WriteLine();
+
+            Console.WriteLine("List<string> para IEnumerable<object>");
+
+            IEnumerable<object> enumObj = listaMeses; // Covariância
+            foreach (var item in enumObj)
+            {
+                Console.WriteLine(item);
+            }
+
+            // IEnumerable não permite alterar o dado de entrada bem como acessar via índice
+        }
+    }
+}
---
+++ C#/Collections-2/Alura-Collections_Parte_2/Foreach/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var meses = new List<string>()
+            {
+                "Janeiro", "Fevereiro", "Março", "Abril",
+                "Maio", "Junho", "Julho", "Agosto",
+                "Setembro", "Outubro", "Novembro", "Dezembro",
+            };
+
+            var mesesArray = new string[]
+            {
+                "Janeiro", "Fevereiro", "Março", "Abril",
+                "Maio", "Junho", "Julho", "Agosto",
+                "Setembro", "Outubro", "Novembro", "Dezembro",
+            };
+
+            // for each usa o MoveNext() do Enumerator para as classes que implementam a interface
+            foreach (var item in meses)
+            {
+                // item = item.ToUpper(); // Não compila, não aceita alterar o iterador do for each
+                // meses[0] = meses[0].ToUpper(); // Aqui estoura exception indicando que a coleção foi alterada
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in mesesArray)
+            {
+                meses[0] = meses[0].ToUpper(); // Aqui aceita, é como se fosse um laço for
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
