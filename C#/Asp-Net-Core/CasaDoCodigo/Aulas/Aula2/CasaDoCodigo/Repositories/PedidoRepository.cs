b631192e16f73a0c9b6c58fce9fccc4bb74afe88
mario_junior <mario_junior@hst.com.br>
2022-12-07 17:23:13 -0300

    Novas implementações

---
+++ C#/Asp-Net-Core/CasaDoCodigo/Aulas/Aula2/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -0,0 +1,86 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface IPedidoRepository
+    {
+        Pedido GetPedido();
+        void AddItem(string codigo);
+    }
+
+    public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
+    {
+        private readonly IHttpContextAccessor contextAccessor;
+
+        public PedidoRepository(ApplicationContext contexto,
+            IHttpContextAccessor contextAccessor) : base(contexto)
+        {
+            this.contextAccessor = contextAccessor;
+        }
+
+        public void AddItem(string codigo)
+        {
+            var produto = contexto.Set<Produto>()
+                .Where(p => p.Codigo == codigo)
+                .SingleOrDefault();
+
+            if (produto == null)
+            {
+                throw new ArgumentException("Produto não encontrado");
+            }
+
+            var pedido = this.GetPedido();
+
+            var itemPedido = contexto.Set<ItemPedido>()
+                .Where(i => i.Produto.Codigo == codigo
+                && i.Pedido.Id == pedido.Id)
+                .SingleOrDefault();
+
+            if (itemPedido == null)
+            {
+                itemPedido = new ItemPedido(pedido, produto, 1, produto.Preco);
+                contexto.Set<ItemPedido>()
+                    .Add(itemPedido);
+
+                contexto.SaveChanges();
+            }
+        }
+
+        public Pedido GetPedido()
+        {
+            var pedidoId = this.GetPedidoId();
+            var pedido = this.dbSet
+                .Include(p => p.Itens)
+                .ThenInclude(i => i.Produto)
+                .Where(p => p.Id == pedidoId)
+                .SingleOrDefault();
+
+            if (pedido == null)
+            {
+                pedido = new Pedido();
+                this.dbSet.Add(pedido);
+                this.contexto.SaveChanges();
+                this.SetPedidoId(pedido.Id);
+            }
+
+            return pedido;
+        }
+
+        private int? GetPedidoId()
+        {
+            return this.contextAccessor.HttpContext.Session.GetInt32("pedidoId");
+        }
+
+        private void SetPedidoId(int pedidoId)
+        {
+            this.contextAccessor.HttpContext.Session.SetInt32("pedidoId", pedidoId);
+        }
+    }
+
+}
--- C#/Asp-Net-Core/CasaDoCodigo/Aulas/Aula2/CasaDoCodigo/Repositories/ProdutoRepository.cs
+++ C#/Asp-Net-Core/CasaDoCodigo/Aulas/Aula2/CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -1,28 +1,29 @@
 ﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CasaDoCodigo.Repositories
 {
-    public class ProdutoRepository : IProdutoRepository
+    public class ProdutoRepository : BaseRepository<Produto>, IProdutoRepository
     {
-        private readonly ApplicationContext contexto;
-
-        public ProdutoRepository(ApplicationContext contexto)
+        public ProdutoRepository(ApplicationContext contexto) : base(contexto)
         {
-            this.contexto = contexto;
         }
 
         public IList<Produto> GetProdutos()
         {
-            return this.contexto.Set<Produto>().ToList();
+            return this.dbSet.ToList();
         }
 
         public void SaveProdutos(List<Livro> livros)
         {
             foreach (var livro in livros)
             {
-                this.contexto.Set<Produto>().Add(new Produto(livro.Codigo, livro.Nome, livro.Preco));
+                if (!this.dbSet.Where(p => p.Codigo == livro.Codigo).Any())
+                {
+                    this.dbSet.Add(new Produto(livro.Codigo, livro.Nome, livro.Preco));
+                }
             }
 
             this.contexto.SaveChanges();
