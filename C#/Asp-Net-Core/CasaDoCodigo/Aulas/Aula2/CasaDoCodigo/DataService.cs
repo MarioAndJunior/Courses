92cc7162791fbd1806a875afad91a6e1b2564ffc
MarioAndJunior <majr85@gmail.com>
2022-12-07 07:49:50 -0300

    Rollback para o .net 2.0 para poder acompanhar o curso

---
+++ C#/Asp-Net-Core/CasaDoCodigo/Aulas/Aula2/CasaDoCodigo/DataService.cs
@@ -0,0 +1,34 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CasaDoCodigo
+{
+    public class DataService : IDataService
+    {
+        private readonly ApplicationContext contexto;
+        private readonly IProdutoRepository produtoRepository;
+
+        public DataService(ApplicationContext contexto, IProdutoRepository produtoRepository)
+        {
+            this.contexto = contexto;
+            this.produtoRepository = produtoRepository;
+        }
+
+        public void InicializaDb()
+        {
+            contexto.Database.EnsureCreated();
+            List<Livro> livros = GetLivros();
+            this.produtoRepository.SaveProdutos(livros);
+        }
+
+        private static List<Livro> GetLivros()
+        {
+            string json = File.ReadAllText("livros.json");
+            List<Livro> livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            return livros;
+        }
+    }
+}
---
+++ C#/Asp-Net-Core/CasaDoCodigo/Aulas/Aula2/CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -0,0 +1,31 @@
+﻿using CasaDoCodigo.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly ApplicationContext contexto;
+
+        public ProdutoRepository(ApplicationContext contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        public IList<Produto> GetProdutos()
+        {
+            return this.contexto.Set<Produto>().ToList();
+        }
+
+        public void SaveProdutos(List<Livro> livros)
+        {
+            foreach (var livro in livros)
+            {
+                this.contexto.Set<Produto>().Add(new Produto(livro.Codigo, livro.Nome, livro.Preco));
+            }
+
+            this.contexto.SaveChanges();
+        }
+    }
+}
