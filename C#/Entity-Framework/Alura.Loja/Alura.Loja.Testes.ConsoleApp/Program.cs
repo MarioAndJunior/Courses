b98bb0d9bac62188686d1b1f7e82ffab2b67af2b
mario_junior <mario_junior@hst.com.br>
2022-12-09 17:53:16 -0300

    :sparkles: Curso de EF

---
+++ C#/Entity-Framework/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GravarUsandoAdoNet();
+            //GravarUsandoEntity();
+            //RecuperarProdutos();
+            //ExcluirProdutos();
+            AtualizarProduto();
+        }
+
+        private static void AtualizarProduto()
+        {
+            ExcluirProdutos();
+            // incluir
+            GravarUsandoEntity();
+            RecuperarProdutos();
+
+            // atualizar
+            using (var repo = new ProdutoDAOEntity())
+            {
+                Produto produto = repo.Produtos().First();
+
+                produto.Nome = "Cassino Royale editado";
+                repo.Atualizar(produto);
+            }
+            RecuperarProdutos();
+        }
+
+        private static void ExcluirProdutos()
+        {
+            using (var repo = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = repo.Produtos();
+
+                foreach (var item in produtos)
+                {
+                    repo.Remover(item);
+                }
+
+                RecuperarProdutos();
+            }
+        }
+
+        private static void RecuperarProdutos()
+        {
+            using (var repo = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = repo.Produtos(); // Mesmo que select * from
+                Console.WriteLine($"Foram encontrados {produtos.Count} produtos");
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item.Nome);
+                }
+            }
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Cassino Royale";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                contexto.Adicionar(p);
+            }
+        }
+
+        private static void GravarUsandoAdoNet()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var repo = new ProdutoDAO())
+            {
+                repo.Adicionar(p);
+            }
+        }
+    }
+}
