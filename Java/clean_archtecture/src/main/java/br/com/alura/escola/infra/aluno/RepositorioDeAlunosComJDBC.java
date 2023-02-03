24a0edd1352ec5d7002bba98face4b66ea9af4d2
mario_junior <mario_junior@hst.com.br>
2023-02-03 18:23:53 -0300

    cursp de clean arch

---
+++ Java/clean_archtecture/src/main/java/br/com/alura/escola/infra/aluno/RepositorioDeAlunosComJDBC.java
@@ -0,0 +1,119 @@
+package br.com.alura.escola.infra.aluno;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.alura.escola.dominio.aluno.Aluno;
+import br.com.alura.escola.dominio.aluno.CPF;
+import br.com.alura.escola.dominio.aluno.Email;
+import br.com.alura.escola.dominio.aluno.RepositorioDeAlunos;
+import br.com.alura.escola.dominio.aluno.Telefone;
+
+public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos {
+	
+	private final Connection connection;
+	
+	public RepositorioDeAlunosComJDBC(Connection connection) {
+		this.connection = connection;
+	}
+
+	@Override
+	public void matricular(Aluno aluno) {
+		try {
+			String sql = "INSERT INTO ALUNO VALUES(?, ?, ?)";
+			PreparedStatement ps = connection.prepareStatement(sql);
+			
+			ps.setString(1, aluno.getCpf().getNumero());
+			ps.setString(2, aluno.getNome());
+			ps.setString(3, aluno.getEmail().getEndereco());
+			ps.execute();
+			
+			sql = "INSERT INTO TELEFONE VALUES(?, ?)";
+			ps = connection.prepareStatement(sql);
+			
+			for (Telefone telefone: aluno.getTelefones()) {
+				ps.setString(1, telefone.getDdd());
+				ps.setString(2, telefone.getNumero());
+				ps.execute();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@Override
+	public Aluno buscarPorCPF(CPF cpf) {
+		try {
+			
+			String sql = "SELECT id, nome, email FROM ALUNO WHERE cpf = ?";
+			PreparedStatement ps;
+			ps = connection.prepareStatement(sql);
+			ps.setString(1, cpf.getNumero());
+			ResultSet result = ps.executeQuery();
+			
+			boolean encontrou = result.next();
+			if (!encontrou) {
+				throw new AlunoNaoEncontrado(cpf);
+			}
+			
+			String nome = result.getString("nome");
+			Email email = new Email(result.getString("email"));
+			Aluno encontrado = new Aluno(cpf, nome, email);
+			
+			Long id = result.getLong("id");
+			sql = "SELECT ddd, numero FROM TELEFONE WHERE aluno = ?";
+			ps = connection.prepareStatement(sql);
+			ps.setLong(1, id);
+			result = ps.executeQuery();
+			
+			while (result.next()) {
+				String numero = result.getString("numero");
+				String ddd = result.getString("ddd");
+				encontrado.adicionarTelefone(ddd, numero);
+			}
+			
+			return encontrado;
+		} catch (SQLException | AlunoNaoEncontrado e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public List<Aluno> listarTodosAlunosMatriculados() {
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		try {
+			String sql = "SELECT id, cpf, nome, email FROM ALUNO";
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ResultSet result = ps.executeQuery();
+			
+			while (result.next()) {
+				CPF cpf = new CPF(result.getString("cpf"));
+				String nome = result.getString("nome");
+				Email email = new Email(result.getString("email"));
+				Aluno encontrado = new Aluno(cpf, nome, email);
+				
+				Long id = result.getLong("id");
+				sql = "SELECT ddd, numero FROM TELEFONE WHERE aluno_id = ?";
+				ps = connection.prepareStatement(sql);
+				ps.setLong(1, id);
+				ResultSet telefones = ps.executeQuery();
+				
+				while (telefones.next()) {
+					String numero = telefones.getString("numero");
+					String ddd = telefones.getString("ddd");
+					encontrado.adicionarTelefone(ddd, numero);
+				}
+				alunos.add(encontrado);
+			}
+			
+			return alunos;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ Java/clean_archtecture/src/test/java/br/com/alura/escola/dominio/aluno/TelefoneTest.java
@@ -0,0 +1,25 @@
+package br.com.alura.escola.dominio.aluno;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.escola.dominio.aluno.Telefone;
+
+class TelefoneTest {
+
+	@Test
+	void naoDeveriaCriarTelefonesComNumerosInvalidos() {
+		assertThrows(IllegalArgumentException.class,
+				() -> new Telefone(null, null));
+		assertThrows(IllegalArgumentException.class,
+				() -> new Telefone("1", "998989898"));
+		assertThrows(IllegalArgumentException.class,
+				() -> new Telefone("19", "999"));
+	}
+	
+	@Test
+	void deveriaCriarTelefoneComNumerosValidos() {
+		assertDoesNotThrow(() -> new Telefone("19", "998989898"));
+	}
+}
