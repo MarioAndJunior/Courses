95a946276e37c85d80b8a8cb3f2d34c1f1b57835
MarioAndJunior <majr85@gmail.com>
2024-05-06 23:15:50 -0300

    :sparkles: novos capitulos do modulo

--- Android-Kotlin/android-com-kotlin-migrations-e-relacionamento-com-room/app/src/main/java/br/com/alura/orgs/ui/activity/FormularioCadastroUsuarioActivity.kt
+++ Android-Kotlin/android-com-kotlin-migrations-e-relacionamento-com-room/app/src/main/java/br/com/alura/orgs/ui/activity/FormularioCadastroUsuarioActivity.kt
@@ -2,14 +2,23 @@ package br.com.alura.orgs.ui.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import br.com.alura.orgs.database.AppDatabase
 import br.com.alura.orgs.databinding.ActivityFormularioCadastroUsuarioBinding
+import br.com.alura.orgs.extensions.toHash
 import br.com.alura.orgs.model.Usuario
+import kotlinx.coroutines.launch
 
 class FormularioCadastroUsuarioActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityFormularioCadastroUsuarioBinding.inflate(layoutInflater)
+    }
+
+    private val dao by lazy {
+        AppDatabase.instancia(this).usuarioDao()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,14 +31,25 @@ class FormularioCadastroUsuarioActivity : AppCompatActivity() {
         binding.activityFormularioCadastroBotaoCadastrar.setOnClickListener {
             val novoUsuario = criaUsuario()
             Log.i("CadastroUsuario", "onCreate: $novoUsuario")
-            finish()
+            lifecycleScope.launch{
+                try {
+                    dao.salva(novoUsuario)
+                    finish()
+                } catch (e: Exception) {
+                    Log.e("CadastroUsuario", "Erro ao cadastrar usuario", e)
+                    Toast.makeText(this@FormularioCadastroUsuarioActivity,
+                        "Falha ao cadastrar usuário",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
     private fun criaUsuario(): Usuario {
         val usuario = binding.activityFormularioCadastroUsuario.text.toString()
         val nome = binding.activityFormularioCadastroNome.text.toString()
-        val senha = binding.activityFormularioCadastroSenha.text.toString()
+        val senha = binding.activityFormularioCadastroSenha.text.toString().toHash()
+
         return Usuario(usuario, nome, senha)
     }
 }
