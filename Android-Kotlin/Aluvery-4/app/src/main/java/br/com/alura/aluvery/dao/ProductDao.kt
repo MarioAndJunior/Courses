9847f0366610b9b5c28b24f07740294de4c58bc7
mario_junior <mario_junior@hst.com.br>
2024-07-05 10:01:42 -0300

    :sparkles: terminando a formação básica de Compose

---
+++ Android-Kotlin/Aluvery-4/app/src/main/java/br/com/alura/aluvery/dao/ProductDao.kt
@@ -0,0 +1,19 @@
+package br.com.alura.aluvery.dao
+
+import androidx.compose.runtime.mutableStateListOf
+import br.com.alura.aluvery.model.Product
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class ProductDao {
+
+    companion object {
+        private val products = MutableStateFlow<List<Product>>(emptyList())
+    }
+    fun products() = products.asStateFlow()
+
+    fun save(product: Product) {
+        products.value += product
+    }
+
+}
