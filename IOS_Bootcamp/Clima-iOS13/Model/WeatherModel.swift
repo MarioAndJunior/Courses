7bfa13e83fd2314c6ed655c1f8b94c751d74c2b7
mario_junior <mario_junior@hst.com.br>
2024-12-16 17:03:05 -0300

    Protocols lesson

---
+++ IOS_Bootcamp/Clima-iOS13/Model/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by HST on 11/11/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ IOS_Bootcamp/Protocols/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by HST on 14/10/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
