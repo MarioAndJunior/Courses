7ccb999095bf9183438bac1ce826b907b8cf9c50
MarioAndJunior <majr85@gmail.com>
2025-03-18 07:56:36 -0300

    finalizando primeiro modulo React

--- React/organo/organo/src/componentes/Time/index.js
+++ React/organo/organo/src/componentes/Time/index.js
@@ -8,7 +8,8 @@ const Time = (props) => {
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
                 {props.colaboradores.map(colaborador => 
-                <Colaborador 
+                <Colaborador key={colaborador.nome}
+                    corDeFundo={props.corPrimaria}
                     nome={colaborador.nome} 
                     cargo={colaborador.cargo}
                     imagem={colaborador.imagem}
